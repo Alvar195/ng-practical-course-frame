f80d3a8501aacc5ec2009feedebc6532855ab98f
Alvar Sokk <alvar195@hotmail.com>
2019-07-08 17:13:51 +0300

    Step 4

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,18 @@ import { AuthComponent } from './containers/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignUpComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule
   ],
-  exports: [AuthComponent],
+  exports: [AuthComponent, LoginComponent]
 })
-export class AuthModule { }
+export class AuthModule {}
